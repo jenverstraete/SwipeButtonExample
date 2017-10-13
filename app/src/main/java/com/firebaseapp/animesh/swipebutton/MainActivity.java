72878bd3a0b86dd6b99e59d251abd1d3d0e50193
JenVerstraete <jenverstraete@vanmarcke.be>
2017-10-13 13:12:36 +0200

    Simplified the code a lot and made my own version of the slider button that supports left/right swipe and updates the text from positive to negative

--- app/src/main/java/com/firebaseapp/animesh/swipebutton/MainActivity.java
+++ app/src/main/java/com/firebaseapp/animesh/swipebutton/MainActivity.java
@@ -1,8 +1,7 @@
 package com.firebaseapp.animesh.swipebutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,26 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SwipeButton mSwipeButton = (SwipeButton) findViewById(R.id.my_swipe_button);
-
-        SwipeButtonCustomItems swipeButtonSettings = new SwipeButtonCustomItems() {
-            @Override
-            public void onSwipeConfirm() {
-                Log.d("NEW_STUFF", "New swipe confirm callback");
-            }
-        };
-
-        swipeButtonSettings
-                .setButtonPressText(">> NEW TEXT! >>")
-                .setGradientColor1(0xFF888888)
-                .setGradientColor2(0xFF666666)
-                .setGradientColor2Width(60)
-                .setGradientColor3(0xFF333333)
-                .setPostConfirmationColor(0xFF888888)
-                .setActionConfirmDistanceFraction(0.7)
-                .setActionConfirmText("Action Confirmed");
-
-        if (mSwipeButton != null) {
-            mSwipeButton.setSwipeButtonCustomItems(swipeButtonSettings);
-        }
     }
 }
